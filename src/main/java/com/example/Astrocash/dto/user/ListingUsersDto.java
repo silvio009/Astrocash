6e4f50c8706fefd83b2048fa2b51fa346dbd9e56
silvio009 <rm550821@fiap.com>
2025-09-18 13:04:53 -0300

    updating userDtos

--- src/main/java/com/example/Astrocash/dto/user/ListingUsersDto.java
+++ src/main/java/com/example/Astrocash/dto/user/ListingUsersDto.java
@@ -1,11 +1,14 @@
 package com.example.Astrocash.dto.user;
 
 import com.example.Astrocash.models.users.Email;
+import com.example.Astrocash.models.users.Endereco;
 import com.example.Astrocash.models.users.Senha;
 import com.example.Astrocash.models.users.User;
 
-public record ListingUsersDto(String id, String nome, Email email, Senha senha, String role) {
+import java.util.Date;
+
+public record ListingUsersDto(String id, String nome, Email email, Senha senha, String role, String cpf, Date dataCadastro, String telefone, Endereco endereco) {
     public ListingUsersDto(User user){
-        this(user.getId(), user.getNome(),user.getEmail(), user.getSenha(), user.getRole());
+        this(user.getId(), user.getNome(),user.getEmail(), user.getSenha(), user.getRole(), user.getCpf(),user.getDataCadastro(),user.getTelefone(),user.getEndereco());
     }
 }
